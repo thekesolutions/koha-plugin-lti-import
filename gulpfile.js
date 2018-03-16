dec2355a090ab7dc41cf465360377b1a786a775c
Tomas Cohen Arazi <tomascohen@theke.io>
2018-03-16 09:48:06 -0300

    Fix gulpfile
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,8 @@ const today = dt.format('Y-m-d');
 const package_json = JSON.parse(fs.readFileSync('./package.json'));
 const release_filename = `${package_json.name}-v${package_json.version}.kpz`;
 
-const pm_file = 'KitchenSink.pm';
-const pm_file_path = 'Koha/Plugin/Com/ByWaterSolutions/';
+const pm_file = 'LTIImport.pm';
+const pm_file_path = 'Koha/Plugin/Com/ThekeSolutions/';
 const pm_file_path_full = pm_file_path + pm_file;
 const pm_file_path_dist = 'dist/' + pm_file_path;
 const pm_file_path_full_dist = pm_file_path_dist + pm_file;
